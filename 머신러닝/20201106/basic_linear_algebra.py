601e558ef364c1d659817ed7c80a6765f6e68974
hahacandy <58638224+hahacandy@users.noreply.github.com>
2020-11-09 21:16:53 +0900

    1

--- 머신러닝/20201106/basic_linear_algebra.py
+++ 머신러닝/20201106/basic_linear_algebra.py
@@ -69,13 +69,16 @@ def scalar_matrix_product(alpha, matrix_variable):
 
 
 def is_product_availability_matrix(matrix_a, matrix_b):
-    return None
+    return True
 
 
 def matrix_product(matrix_a, matrix_b):
     if is_product_availability_matrix(matrix_a, matrix_b) == False:
         raise ArithmeticError
-    return None
+    else:
+        result = [[sum(aa*bb for aa, bb in zip(a, b)) for b in zip(*matrix_b)]for a in matrix_a]
+
+    return result
 
 if __name__ == "__main__":
     # print(vector_size_check([1, 2, 3], [2, 3, 4], [5, 6, 7]))  # Expected value: True
@@ -121,11 +124,34 @@ if __name__ == "__main__":
     # matrix_w = [[2, 5], [1, 1], [2, 2]]
     # print(matrix_transpose(matrix_w))
 
-    matrix_x = [[2, 2], [2, 2], [2, 2]]
-    matrix_y = [[2, 5], [2, 1]]
-    matrix_z = [[2, 4], [5, 3]]
-    matrix_w = [[2, 5], [1, 1], [2, 2]]
-    print(scalar_matrix_product(3, matrix_x))  # Expected value: [[6, 6], [6, 6], [6, 6]]
-    print(scalar_matrix_product(2, matrix_y))  # Expected value: [[4, 10], [4, 2]]
-    print(scalar_matrix_product(4, matrix_z))  # Expected value: [[8, 16], [20, 12]]
-    print(scalar_matrix_product(3, matrix_w))  # Expected value: [[6, 15], [3, 3], [6, 6]]
+    # matrix_x = [[2, 2], [2, 2], [2, 2]]
+    # matrix_y = [[2, 5], [2, 1]]
+    # matrix_z = [[2, 4], [5, 3]]
+    # matrix_w = [[2, 5], [1, 1], [2, 2]]
+    # print(scalar_matrix_product(3, matrix_x))  # Expected value: [[6, 6], [6, 6], [6, 6]]
+    # print(scalar_matrix_product(2, matrix_y))  # Expected value: [[4, 10], [4, 2]]
+    # print(scalar_matrix_product(4, matrix_z))  # Expected value: [[8, 16], [20, 12]]
+    # print(scalar_matrix_product(3, matrix_w))  # Expected value: [[6, 15], [3, 3], [6, 6]]
+
+    #
+    # matrix_x = [[2, 5], [1, 1]]
+    # matrix_y = [[1, 1, 2], [2, 1, 1]]
+    # matrix_z = [[2, 4], [5, 3], [1, 3]]
+    # print(is_product_availability_matrix(matrix_y, matrix_z))  # Expected value: True
+    # print(is_product_availability_matrix(matrix_z, matrix_x))  # Expected value: True
+    # print(is_product_availability_matrix(matrix_z, matrix_w))  # Expected value: False //matrix_w가없습니다
+    # print(is_product_availability_matrix(matrix_x, matrix_x))  # Expected value: True
+
+    # matrix_x = [[2, 5], [1, 1]]
+    # matrix_y = [[1, 1, 2], [2, 1, 1]]
+    # matrix_z = [[2, 4], [5, 3], [1, 3]]
+    # print(matrix_product(matrix_y, matrix_z))  # Expected value: [[9, 13], [10, 14]]
+    # print(matrix_product(matrix_z, matrix_x))  # Expected value: [[8, 14], [13, 28], [5, 8]]
+    # print(matrix_product(matrix_x, matrix_x))  # Expected value: [[9, 15], [3, 6]]
+    # print(matrix_product(matrix_z, matrix_w))  # Expected value: False
+
+
+    # test
+    matrix_y = [[1, 1, 2], [2, 1, 1]]
+    matrix_z = [[2,5,1], [4,3,3]]
+    print([a for a in zip(matrix_y, matrix_z)])

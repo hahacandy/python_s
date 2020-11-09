9da722443b7daff45072fc0003fdede5443a4363
hahacandy <58638224+hahacandy@users.noreply.github.com>
2020-11-09 13:27:22 +0900

    1

--- 머신러닝/20201106/basic_linear_algebra.py
+++ 머신러닝/20201106/basic_linear_algebra.py
@@ -1,14 +1,27 @@
 def vector_size_check(*vector_variables):
-    return None
+    result = []
+    for vector_variable in vector_variables:
+        if(len(vector_variables[0]) == len(vector_variable)):
+            result.append(1)
+        else:
+            result.append(0)
+    return all(result)
+
 
 def vector_addition(*vector_variables):
-    return None
+    if vector_size_check(*vector_variables):
+        result = [sum(t) for t in zip(*vector_variables)]
+        return result
+    else:
+        raise ArithmeticError
 
 
 def vector_subtraction(*vector_variables):
-    if vector_size_check(*vector_variables) == False:
+    if vector_size_check(*vector_variables):
+        result = [t[0]-sum([a for idx, a in enumerate(t) if idx > 0]) for idx, t in enumerate(zip(*vector_variables))]
+        return result
+    else:
         raise ArithmeticError
-    return None
 
 
 def scalar_vector_product(alpha, vector_variable):
@@ -51,3 +64,15 @@ def matrix_product(matrix_a, matrix_b):
     if is_product_availability_matrix(matrix_a, matrix_b) == False:
         raise ArithmeticError
     return None
+
+if __name__ == "__main__":
+    # print(vector_size_check([1, 2, 3], [2, 3, 4], [5, 6, 7]))  # Expected value: True
+    # print(vector_size_check([1, 3], [2, 4], [6, 7]))  # Expected value: True
+    # print(vector_size_check([1, 3, 4], [4], [6, 7]))  # Expected value: False
+
+    # print(vector_addition([1, 3], [2, 4], [6, 7]))  # Expected value: [9, 14]
+    # print(vector_addition([1, 5], [10, 4], [4, 7]))  # Expected value: [15, 16]
+    # print(vector_addition([1, 3, 4], [4], [6, 7]))  # Expected value: ArithmeticError
+
+    print(vector_subtraction([1, 3], [2, 4]))  # Expected value: [-1, -1]
+    print(vector_subtraction([1, 5], [10, 4], [4, 7]))  # Expected value: [-13, -6]
